395af3219f1a8b285e0b7570c40dcd32369e1f14
vera branton <veronav77@gmail.com>
2024-02-29 21:37:17 -0500

    my second commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("game"); //to see if it is working//
+
+const options = ("rock", "paper", "scissors");
+
+function getComputerChoice() {
+    console.log("computer-game"); //to see if it is working//
+}
+getComputerChoice();
