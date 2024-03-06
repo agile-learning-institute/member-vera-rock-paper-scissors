4162d73c76397ff3ed414fe36f4f58e587254d80
vera branton <veronav77@gmail.com>
2024-03-06 13:19:14 -0500

    play round

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 console.log("game"); //to see if it is working//
 
-const options = ("rock", "paper", "scissors");
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice() {
     console.log("computer-game"); //to see if it is working//
+
+    const theChoice = options[Math.floor(Math.random() * 3)];
+    return theChoice;
 }
-getComputerChoice();
+console.log(getComputerChoice());
+
+function playRound(playerSelection, computerSelection) {
+
+}
+function checkWinner(playerSelection, computerSelection){
+    if(playerSelection == computerSelection) {
+        return "TIE";
+    }
+}
