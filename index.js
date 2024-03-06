b26445231a838f16b303c58c5cb19467f0e4dee6
vera branton <veronav77@gmail.com>
2024-03-06 13:58:26 -0500

    tested  check winner

--- index.js
+++ index.js
@@ -20,7 +20,18 @@ function checkWinner(playerSelection, computerSelection) {
         (playerSelection == "rock" && computerSelection == "scissors") ||
         (playerSelection == "scissors" && computerSelection == "paper") ||
         (playerSelection == "paper" && computerSelection == "rock")
-    ) { return "Player WINS!"
+    ) {
+        return "Player WINS!";
     }
+    return "Computer WINS!";
 }
+console.log(checkWinner("rock", "rock"));
+console.log(checkWinner("rock", "paper"));
+console.log(checkWinner("rock", "scissors"));
+console.log(checkWinner("paper", "rock"));
+console.log(checkWinner("paper", "paper"));
+console.log(checkWinner("paper", "scissors"));
+console.log(checkWinner("scissors", "rock"));
+console.log(checkWinner("scissors", "paper"));
+console.log(checkWinner("scissors", "scissors"));
 console.log(getComputerChoice());
