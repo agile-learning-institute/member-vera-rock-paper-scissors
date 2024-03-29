ed7cd12294fad786b1afcf269e7d071e25a5a2c8
vera branton <veronav77@gmail.com>
2024-03-28 21:51:49 -0400

    dom manipulation and play game

--- index.js
+++ index.js
@@ -1,29 +1,51 @@
-const options = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
+const winningScore = 5
 
-function getComputerChoice() {
-    const theChoice = options[Math.floor(Math.random() * 3)];
-    return theChoice;
+function computerPlay() {
+    const choices = ['rock', 'paper', 'scissors'];
+    return choices[Math.floor(Math.random() * 3)];
 }
 
-function playRound() {
-    const computerSelection = getComputerChoice();
-    const humanSelection = prompt("Enter Your Selection").toLocaleLowerCase();
-    const theWinner = checkWinner(computerSelection, humanSelection);
-    console.log("Computer Selected", computerSelection, "Human Selection", humanSelection, "The winner is", theWinner);
-     
-}
-function checkWinner(playerSelection, computerSelection) {
-    if (playerSelection == computerSelection) {
-        return "TIE";
-    }
-    else if (
-        (playerSelection == "rock" && computerSelection == "scissors") ||
-        (playerSelection == "scissors" && computerSelection == "paper") ||
-        (playerSelection == "paper" && computerSelection == "rock")
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Tie!';
+    } else if (
+        (playerSelection === 'rock' && computerSelection === 'scissors') ||
+        (playerSelection === 'paper' && computerSelection === 'rock') ||
+        (playerSelection === 'scissors' && computerSelection === 'paper')
     ) {
-        return "Player WINS!";
+        playerScore++;
+        return 'You win!' + playerSelection + 'beats' + computerSelection;
+    } else {
+        computerScore++;
+        return 'You lose!' + computerSelection + 'beats' + playerSelection;
     }
-    return "Computer WINS!";
 }
 
-playGame();
+function displayResult(result) {
+    const resultDiv = document.getElementById('result');
+    resultDiv.textContent = result;
+}
+
+function play(playerSelection) {
+    const computerSelection = computerPlay();
+    const result = playRound(playerSelection, computerSelection);
+    displayResult(result);
+}
+
+function checkWinner() {
+    if (playerScore === winningScore) {
+        displayResult('Congratulations! You win the game!');
+        resetGame();
+    } else if (computerScore === winningScore) {
+        displayResult('Game over! Computer wins the game!');
+        resetGame();
+    }
+ }
+
+ function resetGame() {
+    playerScore = 0;
+    computerScore = 0;
+    updateScores();
+}
